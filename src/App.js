b4a0444b04dcf88ef31d1fd578f35314096975cb
conrad96 <conrad.mugisha@gmail.com>
2021-05-08 14:31:52 +0300

    added authentication

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Post from './Post'
 import Logo from './assets/images/header.png'
 import React, {useState, useEffect} from 'react'
 import {auth, db} from './Firebase'
-import {PersonAdd, ExitToApp} from '@material-ui/icons'
+import {PersonAdd, ExitToApp, Unsubscribe} from '@material-ui/icons'
 import { makeStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
 
@@ -14,9 +14,11 @@ function App()
   
   const [open, setOpen] = useState(false);
 
-  let [email, password, username] = useState('');
+  let email = '';
+  let password = '';
+  let username = '';
 
-  const setUser = useState(null);
+  const [user, setUser] = useState(null);
 
   function rand() 
   {
@@ -42,7 +44,7 @@ function App()
       position: 'absolute',
       width: 400,
       backgroundColor: theme.palette.background.paper,
-      border: '2px solid red',
+      border: '2px solid lightgray',
       boxShadow: theme.shadows[5],
       padding: theme.spacing(2, 4, 3),
     },
@@ -70,17 +72,24 @@ function App()
   }, [])
 
   useEffect(()=> {
-    auth.onAuthStateChanged((authuser)=> {
+    const unsubscribe = auth.onAuthStateChanged((authuser)=> {
       if(authuser)
       {
         //user logged in
-        setUser(authuser);
+        console.log(authuser);
+
+        setUser(authuser);      
       }else {
         //user logged out
-        setUser(null);
+        setUser = null;
+      }
+
+      return ()=> {
+        //cleanup
+        unsubscribe();
       }
     })
-  }, [])
+  }, [user, username])
 
   function setEmail(value)
   {
@@ -94,33 +103,39 @@ function App()
 
   function setPassword(value)
   {
-    password = value
+    password = value;
   }
 
-  function handleLogin()
+  function handleLogin(event)
   {
-    return '';
+    event.preventDefault();
   }
 
   const signup = (event) =>
   {
     event.preventDefault();
     //authentication
-    auth.createUserWithEmailAndPassword(email, password).catch((error)=> alert(error.message));
+    console.log(email);
+    console.log(password);
+    auth.createUserWithEmailAndPassword(email, password).then((authUser)=> {
+      authUser.user.updateProfile({
+        displayName: username
+      })
+    }).catch((error)=> alert(error.message));
   }
   
   return (
     <div className="app">
       <Modal open={open} onClose={handleClose} >
-        <div style={modalStyle} className={classes.paper}>
-          <h2 id="simple-modal-title">Signup</h2>
-          <br />
-          <img src={Logo} className="app__headerImage" />
-          <form className="app__signup">            
-            <input placeholder="Username" type="text" value={username} onChange={(event)=> setUsername(event.target.value)} />
-            <input placeholder="Email" type="email" value={email} onChange={(event)=> setEmail(event.target.value)} />
-            <input placeholder="Password" type="password" value={password} onChange={(event)=> setPassword(event.target.value)} />
-            <button type="submit" className="app__loginBtn" onClick={handleLogin} >Signup</button>
+        <div style={modalStyle} className={classes.paper}>                    
+          <form className="app__signup">
+            <h2 id="simple-modal-title">
+              <img src={Logo} className="app__headerImage" />
+            </h2>
+            <input placeholder="Username" type="text" onChange={(event)=> setUsername(event.target.value)} />
+            <input placeholder="Email" type="email" onChange={(event)=> setEmail(event.target.value)} />
+            <input placeholder="Password" type="password" onChange={(event)=> setPassword(event.target.value)} />
+            <button type="submit" className="app__signupBtn" onClick={(event)=> signup(event)} >Signup</button>
           </form>
         </div>
       </Modal>
